36b6875e0b138650528d24b7a3291bb6593e09ab
JimmyHernandez <jimmy.hernandez-rivera@hobeltonschool.com>
2022-07-10 22:31:17 +0000

    ready to fill

---
+++ func_pk1.c
@@ -0,0 +1,5 @@
+int _stdout(char);
+int print_aux(const char *format, print_f print[], va_list args);
+int print_string(va_list);
+int print_String(va_list);
+int print_pointer(va_list);
---
+++ func_pk2.c
@@ -0,0 +1,5 @@
+int print_int(va_list);
+int print_percentage(va_list);
+int print_unsigned_int(va_list);
+int print_binary(va_list);
+int print_char(va_list);
