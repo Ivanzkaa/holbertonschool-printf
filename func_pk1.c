ed752fdd0efb4d7a7e74b9e96e32809a59a941fa
JimmyHernandez <jimmy.hernandez-rivera@hobeltonschool.com>
2022-07-10 22:41:36 +0000

    gettin ready

--- func_pk1.c
+++ func_pk1.c
@@ -1,5 +1,80 @@
-int _stdout(char);
+#include "main.h"
+
+/**
+*  - 
+* @:
+* @:
+*@:
+* Return: 
+*/
+int _stdout(char)
+{
+
+
+
+
+
+}
+
+/** 
+*  - 
+* @:
+* @:
+*@:
+* Return: 
+*/
+
 int print_aux(const char *format, print_f print[], va_list args);
+{
+ 
+ 
+ 
+ 
+}
+ 
+/** 
+*  - 
+* @:
+* @:
+*@:
+* Return: 
+*/
 int print_string(va_list);
+{
+ 
+ 
+ 
+
+ 
+}
+/** 
+*  - 
+* @:
+* @:
+*@:
+* Return: 
+*/
 int print_String(va_list);
+{
+ 
+ 
+ 
+ 
+ 
+}
+
+/** 
+*  - 
+* @:
+* @:
+*@:
+* Return: 
+*/
 int print_pointer(va_list);
+{
+
+
+ 
+ 
+ 
+}
--- func_pk2.c
+++ func_pk2.c
@@ -1,5 +1,55 @@
-int print_int(va_list);
-int print_percentage(va_list);
-int print_unsigned_int(va_list);
-int print_binary(va_list);
-int print_char(va_list);
+/** 
+*  - 
+*@:
+*@:
+*@:
+* Return: 
+*/
+int print_int(va_list)
+{
+}
+ 
+/** 
+*  - 
+* @:
+* @:
+*@:
+* Return: 
+*/
+int print_percentage(va_list)
+{
+}
+ 
+/** 
+*  - 
+*@:
+*@:
+*@:
+* Return: 
+*/
+int print_unsigned_int(va_list)
+{
+}
+ 
+/** 
+*  - 
+*@:
+*@:
+*@:
+* Return: 
+*/
+int print_binary(va_list)
+{ 
+}
+ 
+/** 
+*  - 
+*@:
+*@:
+*@:
+* Return: 
+*/
+ int print_char(va_list)
+{
+ 
+} 
