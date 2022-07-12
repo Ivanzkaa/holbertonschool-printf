d879ff246328052623439284f01579c7a2088790
Ivanzkaa <ivanzka@me.com>
2022-07-12 03:40:26 +0000

    the functions for the task

---
+++ func_pk3.c
@@ -0,0 +1,175 @@
+#include "main.h"
+/**
+ * print_char - printing a char
+ * @args: arguments
+ * Return: 1
+ **/
+int print_char(va_list *args)
+{
+	char character = va_arg(*args, int);
+
+	write(1, &character, 1);
+	return (0);
+}
+
+
+#include "main.h"
+/**
+ * print_hexadecimal - printing the hexadecimal
+ * @list: list of arguments
+ * Return: the numbers printed
+ **/
+int print_hexadecimal(va_list list)
+{
+	unsigned int num;
+	int len;
+	int rem_num;
+	char *hex_rep;
+	char *rev_hex;
+
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 16);
+	hex_rep = malloc(sizeof(char) * len + 1);
+	if (hex_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		rem_num = num % 16;
+		if (rem_num > 9)
+		{
+			rem_num = hex_check(rem_num, 'x');
+			hex_rep[len] = rem_num;
+		}
+		else
+			hex_rep[len] = rem_num + 48;
+		num = num / 16;
+	}
+	hex_rep[len] = '\0';
+	rev_hex = rev_string(hex_rep);
+	if (rev_hex == NULL)
+		return (-1);
+	write_base(rev_hex);
+	free(hex_rep);
+	free(rev_hex);
+	return (len);
+}
+
+
+#include "main.h"
+/**
+ * print_heXadecimal - printing the hexadecimals uppercase
+ * @list: list of arguments
+ * Return: the numbers printed
+ **/
+int print_heXadecimal(va_list list)
+{
+	unsigned int num;
+	int len;
+	int rem_num;
+	char *hex_rep;
+	char *rev_hex;
+
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 16);
+	hex_rep = malloc(sizeof(char) * len + 1);
+	if (hex_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		rem_num = num % 16;
+		if (rem_num > 9)
+		{
+			rem_num = hex_check(rem_num, 'X');
+			hex_rep[len] = rem_num;
+		}
+		else
+			hex_rep[len] = rem_num + 48;
+		num = num / 16;
+	}
+	hex_rep[len] = '\0';
+	rev_hex = rev_string(hex_rep);
+	if (rev_hex == NULL)
+		return (-1);
+	write_base(rev_hex);
+	free(hex_rep);
+	free(rev_hex);
+	return (len);
+}
+
+
+#include "main.h"
+/**
+ * print_octal - printing numbers in octal base
+ * @list: list of arguments
+ * Return: the number of the output
+ **/
+int print_octal(va_list list)
+{
+	unsigned int num;
+	int len;
+	char *octal_rep;
+	char *rev_str;
+
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (_write_char('0'));
+	if (num < 1)
+		return (-1);
+	len = base_len(num, 8);
+
+	octal_rep = malloc(sizeof(char) * len + 1);
+	if (octal_rep == NULL)
+		return (-1);
+	for (len = 0; num > 0; len++)
+	{
+		octal_rep[len] = (num % 8) + 48;
+		num = num / 8;
+
+	}
+	octal_rep[len] = '\0';
+	rev_str = rev_string(octal_rep);
+	if (rev_str == NULL)
+		return (-1);
+
+	write_base(rev_str);
+	free(octal_rep);
+	free(rev_str);
+	return (len);
+}
+
+
+#include "main.h"
+/**
+ * print_reversed - printing a reversed string
+ * @arg: arguments
+ * Return: the characters printed
+ **/
+int print_reversed(va_list arg)
+{
+	int len;
+	char *str;
+	char *ptr;
+
+	str = va_arg(arg, char *);
+	if (str == NULL)
+		return (-1);
+	ptr = rev_string(str);
+	if (ptr == NULL)
+		return (-1);
+	for (len = 0; ptr[len] != '\0'; len++)
+		_write_char(ptr[len]);
+	free(ptr);
+	return (len);
+}
+
